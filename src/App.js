db2fff8e1a4806592e18ef78e1e7c4dc1c6769bf
Ju640 <67836937+Ju640@users.noreply.github.com>
2021-05-07 15:31:23 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
        <h1>Weather app final project</h1>
          <footer>
            This project was coded by <a href="https://www.linkedin.com/in/judy-dionne-7a5093107/" target="_blank">Judy Dionne</a> and is {" "}
            <a href="https://github.com/Ju640/weatherappfinalproject" target="_blank"> 
            open-sourced on GitHub</a>
         </footer>
+        </div>
     </div>
   );
 }
