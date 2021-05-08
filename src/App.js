aca7c222969f6c43944575e42214c132829b7f9a
Ju640 <67836937+Ju640@users.noreply.github.com>
2021-05-08 10:48:12 -0500

    fixed Netlify errors

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-       
-       <Weather/>
-         <footer>
-           This project was coded by <a href="https://www.linkedin.com/in/judy-dionne-7a5093107/" target="_blank">Judy Dionne</a> and is {" "}
-           <a href="https://github.com/Ju640/weatherappfinalproject" target="_blank"> 
-           open-sourced on GitHub</a>
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/judy-dionne-7a5093107/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Judy Dionne
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Ju640/weatherappfinalproject"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
         </footer>
-        </div>
+      </div>
     </div>
   );
 }
-
-
--- src/Weather.js
+++ src/Weather.js
@@ -2,48 +2,43 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-    return (
-        <div className="Weather">
-            <form>
-                <div className="row">
-                    <div className="col-9">
-                        <input 
-                        type="search" 
-                        placeholder="Enter a city.."
-                        className="form-control"/>
-                    </div>
-                     <div className="col-3">
-                     <input
-                     type="submit"
-                     value="Search"
-                     className="btn btn-primary"/>
-                     </div>
-                </div>
-            </form>
-            <h1>New York</h1>
-            <ul>
-                <li>Friday 16:37</li>
-                <li>Mostly Sunny</li>
-            </ul>
-            <div className="row">
-                <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Sunny"/>
-                    14°C
-                </div>
-                <div className="col-6">
-                    <ul>
-                        <li>
-                            Precipitation: 1%
-                        </li>
-                        <li>
-                            Humidity: 57%
-                        </li>
-                        <li>
-                            Wind: 16 km/h
-                        </li>
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
         </div>
-    )
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Friday 16:37</li>
+        <li>Mostly Sunny</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly Sunny"
+          />
+          14°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 1%</li>
+            <li>Humidity: 57%</li>
+            <li>Wind: 16 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
